0d3a15283b41b518a0e6f142decad3db614fd41a
IlliyaF <yiliy749@gmail.com>
2022-03-24 21:47:14 +0200

    Execute "start"

--- src/TeslaConcriptDb.java
+++ src/TeslaConcriptDb.java
@@ -1,9 +1,9 @@
 public class TeslaConcriptDb {
     public static void main(String[] args) {
 
-
         System.out.println("Hello, Mask!");
         System.out.println("Start");
+        System.out.println("Execute start");
 
     }
 }
